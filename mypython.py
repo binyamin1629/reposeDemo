5853e5b38e5100451f7ed68bb264e77f1b02d4fd
binyamin1629 <binyamindeutschh@gmail.com>
2021-03-04 21:21:47 +0200

    3rd commit

--- mypython.py
+++ mypython.py
@@ -12,3 +12,10 @@ def cls(): return os.system('cls')
 cls()
 print(f"welcom to you {name}")
 print(f"{name.title()} has  {len(name)} letters" )
+
+for index,letter in enumerate(name):
+    print(f"{index+1} {letter}")
+    if letter=="b":
+        print("❤❤❤❤❤❤❤❤❤❤❤❤")
+    else:
+        print("😂❤")    
