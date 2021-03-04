7949f3eecbc1af53af59a441eb7dba0a41c247b9
binyamin1629 <binyamindeutschh@gmail.com>
2021-03-04 20:23:14 +0200

    first commit crated a 01/03/2021

---
+++ mypython.py
@@ -0,0 +1,13 @@
+print("welcome our first github project")
+name=input("what is your name?")
+
+import random
+import os
+
+
+
+
+def cls(): return os.system('cls')
+
+cls()
+print(f"welcom to you {name}")
